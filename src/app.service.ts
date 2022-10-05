dc5b6aa13149b00ce6782893ecfe38ea1fd3b699
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2022-10-06 01:39:36 +0900

    Apply Swagger button on mainPage

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello MofS!';
+    return `Hello MofS! <button onclick="location.href='/swagger'">Swagger</button>`;
   }
 }
