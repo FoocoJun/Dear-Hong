558c7e4b968a8cab731fa25acf4ae332d111aa37
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2022-10-06 01:04:10 +0900

    Apply Swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: INestApplication = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Dear-Hong')
+    .setDescription('이름 제공시 안녕하세요 서비스')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/welcome/welcome.controller.ts
+++ src/welcome/welcome.controller.ts
@@ -1,42 +1,28 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { User } from './interfaces/welcome.interface';
+import {
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+  ApiParam,
+} from '@nestjs/swagger';
 
-/*
-0. hello라는 이름의 API endpoint를 보고 직관적인 이해가 되는가. X
-1. hello라는 이름의 API에 확장성이 있는가. X
-
-*. 해당 사유로 기획된 API 명세서의 변경이 논의 될 수 있는 집단인가. "?"
-
-방안 및 의도
-0. API route 변경 hello -> welcome/message
-결과. welcome/message?name="홍길동" -> {% include 의문1 %}
-
-1. welcome/data 등 접속시 데이터 제공 API 추가 확장 가능.
-*/
-
-// 기존 문제 + 상기 내용 이용한 API 답안.
-// GET /welcome/message?name="홍길동"
 @Controller('welcome')
+@ApiTags('유저 방문 환영합니다 API')
 export class WelcomeController {
-  @Get('message')
-  findAll(@Query('name') name: User['name']): string {
-    console.log(name);
-    return `${name}님. 안녕하세요.`;
-  }
-
-  // 의문0. endPoint는 예시가 아니다.
-  // GET /welcome/message?name="홍길동" -> GET /welcome/message?name={name}
-
-  // 의문1. 데이터를 그대로 사용하는데 필터링을 위한 쿼리를 적용해야하는가.
-  // GET hello?name={name} -> GET hello/{name}
-
-  // 상기 내용 추가 적용한 API 답안.
-  // GET /welcome/message/:name
+  @ApiOperation({
+    summary: '유저에게 환영 메세지를 제공하는 API',
+    description: '사용자 이름 기반 메세지를 제공한다.',
+  })
+  @ApiCreatedResponse({ description: '메세지 생성완료.' })
+  @ApiParam({
+    name: 'name',
+    required: true,
+    description: '메세지에 들어갈 이름',
+    example: '홍길동',
+  })
   @Get('message/:name')
   findOne(@Param('name') name: User['name']): string {
-    console.log(name);
     return `${name}님. 안녕하세요.`;
   }
 }
-
-// Type Script가 아닌 TypeScript입니다.
