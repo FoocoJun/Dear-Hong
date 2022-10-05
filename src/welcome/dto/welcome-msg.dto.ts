beb733ce132e7b577d8efff38abfeee5ae2e96a6
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2022-10-05 23:32:15 +0900

    Improve Questions

--- src/welcome/dto/welcome-msg.dto.ts
+++
@@ -1,3 +0,0 @@
-export class WelcomeMsgDto {
-  name: string;
-}
--- src/welcome/welcome.controller.ts
+++ src/welcome/welcome.controller.ts
@@ -1,19 +1,23 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { User } from './interfaces/welcome.interface';
 
-// 0. hello라는 이름이 API를 보고 받아들일 수 있는가.
-// 1. hello라는 이름이 API 확장성이 있는가.
+/*
+0. hello라는 이름의 API endpoint를 보고 직관적인 이해가 되는가. X
+1. hello라는 이름의 API에 확장성이 있는가. X
 
-// 방안 및 의도
-// API route 변경 hello -> welcome
-// welcome/message?name='홍길동' -> 의문1 추가.
-// 추후 welcome/data 등 접속시 제공 데이터 API 추가 확장 가능.
+*. 해당 사유로 기획된 API 명세서의 변경이 논의 될 수 있는 집단인가. "?"
 
+방안 및 의도
+0. API route 변경 hello -> welcome/message
+결과. welcome/message?name="홍길동" -> {% include 의문1 %}
+
+1. welcome/data 등 접속시 데이터 제공 API 추가 확장 가능.
+*/
+
+// 기존 문제 + 상기 내용 이용한 API 답안.
+// GET /welcome/message?name="홍길동"
 @Controller('welcome')
 export class WelcomeController {
-
-  // 기존 문제 query를 이용한 API 답.
-  // GET /welcome/message?name={name}
   @Get('message')
   findAll(@Query('name') name: User['name']): string {
     console.log(name);
@@ -21,10 +25,12 @@ export class WelcomeController {
   }
 
   // 의문0. endPoint는 예시가 아니다.
-  // GET /hello?name='홍길동' -> GET /hello?name={name}
+  // GET /welcome/message?name="홍길동" -> GET /welcome/message?name={name}
 
-  // 의문1. 데이터를 그대로 사용하는데 필터링을 위한 Query를 적용해야하는가.
+  // 의문1. 데이터를 그대로 사용하는데 필터링을 위한 쿼리를 적용해야하는가.
   // GET hello?name={name} -> GET hello/{name}
+
+  // 상기 내용 추가 적용한 API 답안.
   // GET /welcome/message/:name
   @Get('message/:name')
   findOne(@Param('name') name: User['name']): string {
